ef562a7b7605e1fc224a5d78abe9c0f4102e2d43
restraint444 <restraintrocks@gmail.com>
2025-08-09 19:14:03 +1000

    Initial Commit

---
+++ donotcare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  donotcare
+//
+//  Created by Ethan Tan on 9/8/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ donotcare/donotcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  donotcareApp.swift
+//  donotcare
+//
+//  Created by Ethan Tan on 9/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct donotcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
